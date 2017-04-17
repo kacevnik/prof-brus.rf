6de819de9c315c2b1ec732a07816d3d89b73bc33
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-04-18 01:10:23 +0300

    Добавление маркера на карту

--- includes/find.php
+++ includes/find.php
@@ -16,6 +16,18 @@
 
                             // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
                             map.setCenter(new YMaps.GeoPoint(82.938620, 55.038351), 15);
+
+                                var w = 64;
+                                var h = w/2;
+                                var s = new YMaps.Style();
+                                s.iconStyle = new YMaps.IconStyle();
+                                s.iconStyle.href = "img/pint.png";
+                                s.iconStyle.size = new YMaps.Point(w, w);
+                                s.iconStyle.offset = new YMaps.Point(-h, -w);
+                                var placemark = new YMaps.Placemark(map.getCenter(), {style: s});
+                                placemark.name = "Центр деревянного строительства";
+                                placemark.description = "Мы находимся на ул. Фрунзе 228, оф. 23";
+                                map.addOverlay(placemark);
                         })
         </script>
     </div>
